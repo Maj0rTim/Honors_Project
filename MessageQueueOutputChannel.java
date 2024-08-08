2bc83c8dd2a1f7e1c4af1a1069a4b767ea1da7b0
Timothy Fischer <timfisch242@gmail.com>
2024-08-08 16:14:00 +0200

    message queue progress

--- MessageQueueOutputChannel.java
+++ MessageQueueOutputChannel.java
@@ -1,29 +1,38 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MessageQueueOutputChannel 
 {
     public static final int QUEUE_TYPE = 9999;
-    public static final int MAX_BUF_SIZE = 4096;
 
-    private int msgqid; // The internal message queue identifier
-    private LinuxIPC ipc = new LinuxIPC(); // The native IPC library
-    private byte[] maxBuffer = new byte[MAX_BUF_SIZE];
+    private int msgqid;
+    private ByteBuffer buffer;
+    private LinuxIPC ipc = new LinuxIPC();
     
-    public MessageQueueOutputChannel (int key)
+    public MessageQueueOutputChannel(int key)
     { 
         if ((msgqid = ipc.msgget(key, LinuxIPC.IPC_CREAT | 0660)) == -1)
         System.err.println("MessageQueueOutputStream: msgget failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum()));
-    } // constructor
+    }
 
-    private void sendData (byte[]buf, int len) throws IOException
-    { 
-        if (ipc.msgsnd(msgqid, QUEUE_TYPE, buf, len, 0) == -1)
-          throw new IOException("MessageQueueOutputStream: msgsnd failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum()));
+    private void sendData(ByteBuffer buffer) throws IOException
+    {
+        if (buffer.hasArray()) {
+            if (ipc.msgsnd(msgqid, QUEUE_TYPE, buffer.array(), buffer.position(), 0) == -1)
+              throw new IOException("MessageQueueOutputStream: msgsnd failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum()));
+        } else { throw new IOException("Buffer is empty"); }
     } 
 
-    public void write (byte[] data) throws IOException
+    public void write(byte[] data) throws IOException
     { 
-        
+        buffer = ByteBuffer.wrap(data);
+        sendData(buffer);
+    } 
+
+    public void write(byte[] data, int off, int len) throws IOException
+    { 
+        buffer = ByteBuffer.wrap(data);
+        sendData(buffer.get(data, off, len));
     } 
     
     
