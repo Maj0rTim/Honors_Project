4a22d741c5348e657d677f0156b884f3a9e5cdf9
Maj0rTim <timfisch242@gmail.com>
2024-09-14 13:42:46 +0200

    simulation is working now

--- MessageQueueChannel.java
+++ MessageQueueChannel.java
@@ -5,14 +5,13 @@ public class MessageQueueChannel
 {
     public static final int QUEUE_TYPE = 9999;
     public static final int MAX_BUF_SIZE = 4096;
-    private static final String path = "/home/csgw/JavaProgs/JNI/MessageQueue";
     private int key;
     private int msgqid;
     private int dataSize;
     private ByteBuffer buffer;
     private LinuxNIPC ipc = new LinuxNIPC();
     
-    public MessageQueueChannel() { 
+    public MessageQueueChannel(String path) { 
         if ((key = ipc.ftok(path, 'a')) != -1) { 
             System.out.println("ftok succeeded.  key = " + key);
         } else { System.out.println("ftok failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum())); }
@@ -21,7 +20,11 @@ public class MessageQueueChannel
         buffer = ByteBuffer.allocateDirect(MAX_BUF_SIZE);
     }
 
-    public Long read() throws IOException {
+    public int getMessageQueueID() {
+        return this.msgqid;
+    }
+
+    public Long readLong() throws IOException {
         buffer.clear();
         if ((dataSize = ipc.msgrcv(msgqid, QUEUE_TYPE, buffer, MAX_BUF_SIZE, 0)) == -1) { 
             throw new IOException("MessageQueueInput: read failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum())); 
@@ -31,9 +34,9 @@ public class MessageQueueChannel
         return data;
     }
 
-    public void write(Long data) throws IOException { 
+    public void writeLong(long l) throws IOException { 
         buffer.clear();
-        buffer.putLong(data);
+        buffer.putLong(l);
         if (buffer.hasArray()) {
             if (ipc.msgsnd(msgqid, QUEUE_TYPE, buffer, buffer.capacity(), 0) == -1) {
                 throw new IOException("MessageQueueOutput: msgsnd failed: errnum = " + ipc.getErrnum() + " " + ipc.strerror(ipc.getErrnum()));
@@ -44,7 +47,7 @@ public class MessageQueueChannel
         
     } 
     
-    public void close() throws IOException {
+    public void closeMessageQueue() throws IOException {
         ipc.msgRmid(msgqid);
     }
     
--- MessageQueuePingPong.java
+++ MessageQueuePingPong.java
@@ -1,56 +1,46 @@
 import java.io.IOException;
 
-public class NamedPipePingPong {
+public class MessageQueuePingPong {
     
-    private static final String PING2PONG = "/tmp/fifo_ping2pong";
-    private static final String PONG2PING = "/tmp/fifo_pong2ping";
-    private NamedPipeChannel pipe;
+    private static final String PINGTOPONG = "/home/Timothy/Ping";
+    private static final String PONGTOPING = "/home/Timothy/Pong";
+    private MessageQueueChannel messageQueue;
     private String myName;
+    private int ID;
     private Long Total;
     
-    public NamedPipePingPong(String name) throws IOException {
+    public MessageQueuePingPong(String name) throws IOException {
         this.myName = name;
         this.Total = 0L;
         if (myName.equals("Ping")) {
-            pipe = new NamedPipeChannel(PING2PONG);
+            messageQueue = new MessageQueueChannel(PINGTOPONG);
+            ID = messageQueue.getMessageQueueID();
         } else if (myName.equals("Pong")) {
-            pipe = new NamedPipeChannel(PONG2PING);
+            messageQueue = new MessageQueueChannel(PONGTOPING);
+            ID = messageQueue.getMessageQueueID();
         }
     }
 
     public void playSimulation(int rounds) throws IOException {
-        setChannels();
         synchronize();
         getRoundTripTime(rounds);
-        closeChannels();
-    }
-
-    private void setChannels() throws IOException {
-        if (myName.equals("Ping")) {
-            pipe.setWriteChannel(PING2PONG);
-            pipe.setReadChannel(PONG2PING);
-        } else {
-            pipe.setReadChannel(PING2PONG);
-            pipe.setWriteChannel(PONG2PING);
-        }
     }
 
     private void synchronize() throws IOException {
         if (myName.equals("Ping")) {
-            pipe.writeLong(0);
-            pipe.readLong();
+            messageQueue.writeLong(0L);
+            messageQueue.readLong();
         } else {
-            pipe.readLong();
-            pipe.writeLong(0);
+            messageQueue.readLong();
+            messageQueue.writeLong(0L);
         }
     }
 
     private void getRoundTripTime(int rounds) throws IOException {
         for (int i=0; i<rounds; i++) {
             if (myName.equals("Ping")) {
-                Long start = System.nanoTime();
-                pingThrowBall(start);
-                pingCatchBall();
+                pingThrowBall(System.nanoTime());
+                Long start = pingCatchBall();
                 Long end = System.nanoTime();
                 Total += end - start;
             } else {
@@ -60,25 +50,20 @@ public class NamedPipePingPong {
         System.out.println(Total/rounds);
     }
 
-    private void closeChannels() throws IOException {
-        pipe.closeReadChannel();
-        pipe.closeWriteChannel();
-    }
-
     private void pingThrowBall(Long time) throws IOException {
-        pipe.writeLong(time);
+        messageQueue.writeLong(time);
     }
 
     private Long pingCatchBall() throws IOException {
-        return pipe.readLong();
+        return messageQueue.readLong();
     }
 
     private void pongThrowBall(Long time) throws IOException {
-        pipe.writeLong(time);
+        messageQueue.writeLong(time);
     }
 
     private Long pongCatchBall() throws IOException {
-        return pipe.readLong();
+        return messageQueue.readLong();
     }
     
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -90,7 +75,7 @@ public class NamedPipePingPong {
         String myName = args[0];
         int rounds = Integer.parseInt(args[1]);
         
-        NamedPipePingPong simulation = new NamedPipePingPong(myName);
+        MessageQueuePingPong simulation = new MessageQueuePingPong(myName);
         simulation.playSimulation(rounds);
     }
 }
